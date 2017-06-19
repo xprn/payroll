caee55b5c6f21bbaba0e70c9f33f9d1025ec11bf
Ragnar Laud <ragnar.laud@hotmail.com>
2017-06-19 17:28:29 +0300

    Create the mock system

---
+++ tests/mock/system.js
@@ -0,0 +1,35 @@
+const {System} = require('../../lib/sys');
+
+module.exports = async function () {
+    const Mongoose  = require('mongoose');
+    const Mockgoose = require('mockgoose').Mockgoose;
+    const mockgoose = new Mockgoose(Mongoose);
+
+    const redis    = require('redis-js');
+    const mongoose = Mongoose.connection;
+
+    // Promisify the mock redis connection
+    redis.getAsync  = (key) => new Promise((resolve, reject) =>
+        redis.get(key, (err, data) => err ? reject(err) : resolve(data)));
+    redis.setAsync  = (key, value) => new Promise((resolve, reject) =>
+        redis.set(key, value, err => err ? reject(err) : resolve()));
+    redis.keysAsync = (a) => new Promise((resolve, reject) =>
+        redis.keys(a, (err, keys) => err ? reject(err) : resolve(keys)));
+    redis.delAsync  = (a) => new Promise((resolve, reject) =>
+        redis.del(a, (err, keys) => err ? reject(err) : resolve(keys)));
+
+    // Set the Promise library of mongoose to Bluebird
+    Mongoose.Promise = Promise;
+
+    // Create the mock mongoose connection
+    await mockgoose.prepareStorage();
+    await new Promise((resolve, reject) => {
+        mongoose.on('error', err => reject(err));
+        mongoose.on('open', () => Promise.resolve()
+            .then(() => require('../../lib/sys/mongoose.config')(mongoose))
+            .then(() => resolve()));
+        Mongoose.connect('localhost');
+    });
+
+    return new System({redis, mongoose});
+};
